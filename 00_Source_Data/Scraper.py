f5b7ee72a61c7ba8beb5e09e4d47fb68f80d3ea4
Eric Ríos <70504872+EricR401S@users.noreply.github.com>
2022-12-04 22:55:24 +0000

    Let's eat

---
+++ 00_Source_Data/Scraper.py
@@ -0,0 +1,6 @@
+import requests
+
+
+# url = "http://stats.oecd.org/SDMX-JSON/data/<dataset identifier>/<filter expression>/<agency name>[ ?<additional parameters>]"
+
+# response = requests.get(url)
