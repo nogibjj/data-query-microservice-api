b60c59196f64635f43c24610c0f3b8d16e0cbc25
SongYoungOh <SongYoungOh@MacBook-Pro-76.local>
2022-12-11 10:02:12 -0500

    updated files

--- 10_Code/helper_db.py
+++ 10_Code/helper_db.py
@@ -1,13 +1,19 @@
-# 
-import os
-RDS_HOSTNAME_global_temperatures = os.environ.get("RDS_HOSTNAME")
-RDS_PASSWORD = os.environ.get("RDS_PASSWORD")
-
 import psycopg2
+import os
+
+RDS_HOSTNAME_global_temperatures = str(
+    "global-temperatures.cndtu3jnk9za.us-east-1.rds.amazonaws.com"
+)
+RDS_PASSWORD = "postgres"
+
 
 def connect_to_db():
     connection = psycopg2.connect(
-        host=RDS_HOSTNAME_global_temperatures, port=5432, user="postgres", password=RDS_PASSWORD, database="globaltemperatures"
+        host="global-temperatures.cndtu3jnk9za.us-east-1.rds.amazonaws.com",
+        port=5432,
+        user="postgres",
+        password="postgres",
+        database="globaltemperatures",
     )
     cursor = connection.cursor()
     return cursor
