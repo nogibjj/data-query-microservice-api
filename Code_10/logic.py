20300805a7197f77501432d701afb070f9cc9f5b
Eric Ríos <70504872+EricR401S@users.noreply.github.com>
2022-12-13 18:22:27 +0000

    toyapi branch

--- Code_10/logic.py
+++ Code_10/logic.py
@@ -20,8 +20,25 @@
 #     )
 
 #     return response.json()
-
+import random
 
 def hello_world(to_whom = 'Eldians'):
     to_whom = str(to_whom)
     return f"Goodbye, {to_whom}, for raining the wrath of the Titans upon us."
+
+def ESEP_fun_facts(member='pooja'):
+
+    member = member.lower()
+    E = ['hails from a Ugandan Kingdom', 'gives the best advice when drunk', 'trolls every human at all points in time']
+    S = ['is a classic South Korean Noona',"ADORES SHINHWA and SHINHWA's CHAL SENGIN NAMJA", "was a South Korean Empress in the past life"]
+    P = ['is way too easy to kidnap and take to Broadhead, Gross Hall, and Perkins', "has total turtle + grandma + Bengaluru energy", "is this team's guiding star, for better or worse"]
+
+    fun_facts = {'emmanuel' : E, 'song' : S, 'pooja' : P}
+
+    if member not in fun_facts:
+
+        return "You are not our member, screw off"
+
+    else:
+
+        return member + ' ' + random.choice(fun_facts[member])
--- Makefile
+++ Makefile
@@ -18,7 +18,7 @@ build:
 
 run:
 	#run docker
-	docker run -p 127.0.0.1:8080:8080 562f180f4c23
+	docker run -p 127.0.0.1:8080:8080 9e4cf21dba97
 
 deploy:
  	#deploy
