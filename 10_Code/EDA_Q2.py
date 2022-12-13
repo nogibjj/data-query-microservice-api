66eb644b54077476a900ead1222ca71d74690b89
Emmanuel R <112645280+ruhamyakmc@users.noreply.github.com>
2022-12-13 08:15:58 -0500

    eda for the second part..nothing interesting yet

--- 10_Code/EDA_Q2.py
+++
@@ -1,9 +0,0 @@
-import helper_db
-
-cursor = helper_db.connect_to_db()
-list = []
-cursor.execute(
-"""SELECT * FROM import.globallandtemperaturesbycountry where country = 'Afghanistan';"""
-)
-for i in cursor.fetchall():
-  list.append(i)
