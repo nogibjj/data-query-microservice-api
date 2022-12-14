da42dec2b5ed96905ca81a3c933776b8c755f712
poojakabber7 <112586823+poojakabber7@users.noreply.github.com>
2022-12-14 12:41:04 +0000

    Added streamlit app

--- main/fastapi-apis/api.py
+++ main/fastapi-apis/api.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 import sys
 import os
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
@@ -8,25 +8,26 @@ import eda.eda_q1 as eda
 app = FastAPI()
 
 @app.get("/")
-def root(text):
+def root():
     return {"message": "Hello World"}
 
 @app.get("/trend/{country}")
-def get_country_plot(value: str):
+def get_country_plot(country: str):
     eda.get_country_plot(str(country))
     return {"result": str(country)}
 
 @app.get("/year/{country}/{year}")
-def get_country_year_temp(value: str):
+def get_country_year_temp(country: str, year: str):
     result = eda.get_country_year_temp(str(country), str(year))
     return {"result": str(result)}
 
 @app.get("/city/{country}/{year}")
-def get_city_year_temp(value: str):
+def get_city_year_temp(country: str, year:str):
     result = eda.get_city_year_temp(str(country), str(year))
     return {"result": str(result)}
 
 @app.get("/init/{country}")
-def get_init_temp(value: str):
-    result = eda.get_init_temp(str(country))
-    return {"result": str(result)}
+def get_init_temp(country: str):
+    # result = eda.get_init_temp(str(country))
+    result = [1, 2, 3, 4]
+    return Response(content=result, media_type="application/json")
