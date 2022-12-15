591bc8f06534681865795b4a6b953477c52c6c56
EricR401S <eric.rios.3173@aol.com>
2022-12-14 20:11:01 -0500

    eda 1 function abstracted, dataframe cleaner function for cleaning the columns. We want to take some notes and help from Emmanuel as to tailoring ours.

--- main/eda/eda_q1.py
+++ main/eda/eda_q1.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 import sys
+
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
 sys.path.append(PROJECT_ROOT)
 import helpers
@@ -12,82 +13,109 @@ cursor = helpers.connect_to_db()
 # Question 1: For a given country, what is the trend of their average temperature?
 def get_country_plot(country):
 
-    cursor.execute(f"SELECT * FROM import.globaltemperaturesbycountry where country = '{country}';")
-    country_data = cursor.fetchall()
-    df_country = pd.DataFrame(country_data, columns=[desc[0] for desc in cursor.description])
-    df_country = df_country.sort_values(by=['dt'], ascending=True)
-    df_country = df_country.replace(to_replace='', value = np.nan,regex=True)
-    df_country = df_country.dropna()
-    df_country['year'] = df_country['dt'].str[:4].astype(int)
-    df_country['averagetemperature'] = df_country['averagetemperature'].astype(float)
-    df_country['temperature'] = df_country.groupby('year')['averagetemperature'].transform('mean')
-    df_country.drop_duplicates(
-        subset=["year"], keep="first"
-    ).plot(
-        x="year", y="temperature", kind="line", title="Average Temperature of " + country + ' (by year)'
+    cursor.execute(
+        f"SELECT * FROM import.globaltemperaturesbycountry where country = '{country}' ORDER BY dt ASC;"
     )
+    country_data = cursor.fetchall()
+    df_country = pd.DataFrame(
+        country_data, columns=[desc[0] for desc in cursor.description]
+    )
+    df_country = df_country.replace(to_replace="", value=np.nan, regex=True)
+    # df_country = df_country.sort_values(by=['dt'], ascending=True)
+    df_country = df_country.dropna()
+    df_country["year"] = df_country["dt"].str[:4].astype(int)
+    df_country["averagetemperature"] = df_country["averagetemperature"].astype(float)
+    df_country["temperature"] = df_country.groupby("year")[
+        "averagetemperature"
+    ].transform("mean")
+    df_country.drop_duplicates(subset=["year"], keep="first").plot(
+        x="year",
+        y="temperature",
+        kind="line",
+        title="Average Temperature of " + country + " (by year)",
+    )
+
 
 # Question 2: For a given year and a given country, what is the max/min temperature?
 # TODO: Do the max and min in sql
 def get_country_year_temp(country, year):
-    cursor.execute(f"SELECT * FROM import.globaltemperaturesbycountry where country = '{country}' and dt like '%{year}%';")
+    cursor.execute(
+        f"SELECT MAX(averagetemperature), MIN(averagetemperature), MEAN(averagetemperature) FROM import.globaltemperaturesbycountry where country = '{country}' and dt like '%{year}%';"
+    )
     country_data = cursor.fetchall()
-    df_country = pd.DataFrame(country_data, columns=[desc[0] for desc in cursor.description])
-    df_country = df_country.sort_values(by=['dt'], ascending=True)
-    df_country = df_country.replace(to_replace='', value = np.nan,regex=True)
-    df_country = df_country.dropna()
-    df_country['year'] = df_country['dt'].str[:4].astype(int)
-    df_country['averagetemperature'] = df_country['averagetemperature'].astype(float)
-    max_temp = df_country['averagetemperature'].max()
-    min_temp = df_country['averagetemperature'].min()
-    mean_temp = df_country['averagetemperature'].mean()
+    # df_country = pd.DataFrame(country_data, columns=[desc[0] for desc in cursor.description])
+    # df_country = df_country.sort_values(by=['dt'], ascending=True)
+    # df_country = df_country.replace(to_replace='', value = np.nan,regex=True)
+    # df_country = df_country.dropna()
+    # df_country['year'] = df_country['dt'].str[:4].astype(int)
+    # df_country['averagetemperature'] = df_country['averagetemperature'].astype(float)
+    # max_temp = df_country['averagetemperature'].max()
+    # min_temp = df_country['averagetemperature'].min()
+    # mean_temp = df_country['averagetemperature'].mean()
+    return country_data
 
-    return max_temp, min_temp, mean_temp
 
 # Question 3: For a given year, which city has the highest/lowest temperature on average in a given country?
 # TODO: Try the groupby in sql
 def get_city_year_temp(country, year):
-    cursor.execute(f"SELECT * FROM import.globaltemperaturesbymajorcity where country = '{country}' and dt like '%{year}%';")
+    cursor.execute(
+        f"SELECT city FROM import.globaltemperaturesbymajorcity where country = '{country}' and dt like '%{year}%';"
+    )
     country_data = cursor.fetchall()
-    df_country = pd.DataFrame(country_data, columns=[desc[0] for desc in cursor.description])
-    df_country = df_country.sort_values(by=['dt'], ascending=True)
-    df_country = df_country.replace(to_replace='', value = np.nan,regex=True)
+    df_country = pd.DataFrame(
+        country_data, columns=[desc[0] for desc in cursor.description]
+    )
+    df_country = df_country.sort_values(by=["dt"], ascending=True)
+    df_country = df_country.replace(to_replace="", value=np.nan, regex=True)
     df_country = df_country.dropna()
-    df_country['year'] = df_country['dt'].str[:4].astype(int)
-    df_country['averagetemperature'] = df_country['averagetemperature'].astype(float)
-    df_country['temperature'] = df_country.groupby(['year', 'city'])['averagetemperature'].transform('mean')
-    highest = df_country.sort_values(by=['temperature'], ascending=False).head(1)
-    lowest = df_country.sort_values(by=['temperature'], ascending=True).head(1)
-    highest_city = highest['city'].values[0]
-    lowest_city = lowest['city'].values[0]
+    df_country["year"] = df_country["dt"].str[:4].astype(int)
+    df_country["averagetemperature"] = df_country["averagetemperature"].astype(float)
+    df_country["temperature"] = df_country.groupby(["year", "city"])[
+        "averagetemperature"
+    ].transform("mean")
+    highest = df_country.sort_values(by=["temperature"], ascending=False).head(1)
+    lowest = df_country.sort_values(by=["temperature"], ascending=True).head(1)
+    highest_city = highest["city"].values[0]
+    lowest_city = lowest["city"].values[0]
     return highest_city, lowest_city
+
 
 # Question 4: For a given country, what is the initial and the most recent temperature?
 # TODO: Get the first date and last date in sql
 def get_init_temp(country):
-    cursor.execute(f"SELECT * FROM import.globaltemperaturesbycountry where country = '{country}';")
+    cursor.execute(
+        f"SELECT * FROM import.globaltemperaturesbycountry where country = '{country}';"
+    )
     country_data = cursor.fetchall()
-    df_country = pd.DataFrame(country_data, columns=[desc[0] for desc in cursor.description])
-    df_country = df_country.sort_values(by=['dt'], ascending=True)
-    df_country = df_country.replace(to_replace='', value = np.nan,regex=True)
+    df_country = pd.DataFrame(
+        country_data, columns=[desc[0] for desc in cursor.description]
+    )
+    df_country = df_country.sort_values(by=["dt"], ascending=True)
+    df_country = df_country.replace(to_replace="", value=np.nan, regex=True)
     df_country = df_country.dropna()
-    df_country['year'] = df_country['dt'].str[:4].astype(int)
-    df_country['month'] = df_country['dt'].str[5:7].astype(int)
-    df_country['averagetemperature'] = df_country['averagetemperature'].astype(float)
-    initial = str(df_country.loc[0, 'averagetemperature'])
-    initial_month = str(df_country.loc[0, 'year']) + '-' + str(df_country.loc[0, 'month'])
-    recent = str(df_country.iloc[-1]['averagetemperature'])
-    recent_month = str(df_country.iloc[-1]['year']) + '-' + str(df_country.iloc[-1]['month'])
+    df_country["year"] = df_country["dt"].str[:4].astype(int)
+    df_country["month"] = df_country["dt"].str[5:7].astype(int)
+    df_country["averagetemperature"] = df_country["averagetemperature"].astype(float)
+    initial = str(df_country.loc[0, "averagetemperature"])
+    initial_month = (
+        str(df_country.loc[0, "year"]) + "-" + str(df_country.loc[0, "month"])
+    )
+    recent = str(df_country.iloc[-1]["averagetemperature"])
+    recent_month = (
+        str(df_country.iloc[-1]["year"]) + "-" + str(df_country.iloc[-1]["month"])
+    )
 
     resp = f"The temperature on {initial_month} was {initial} and on {recent_month} was {recent}."
 
     return resp
+
 
 def main():
 
     # get_country_plot("Europe")
     temps = get_init_temp("Afghanistan")
     print(temps)
+
 
 if __name__ == "__main__":
     main()
